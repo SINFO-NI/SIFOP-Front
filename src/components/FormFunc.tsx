a6d8e63cf61746ab4ec5bc2bad293a87639774f2
MarianaPuppin <mpc.marianapuppin@gmail.com>
2021-03-23 19:19:46 -0300

    Form inputs iniciais

--- src/components/FormFunc.tsx
+++ src/components/FormFunc.tsx
@@ -12,10 +12,53 @@ export function FormFunc() {
         
         <div className={styles.formContainer}>
 
-            <form action="" method="POST">
-                <label htmlFor="nome">Nome: </label>
-                <input type="text" id="nome"/>
+            <form action="" method="post">
+                <div>
+                    <label for="nome">Nome:</label>
+                    <input type="text" id="nome" />
+                </div>
+                <div>
+                    <label for="nasc">Data de nascimento:</label>
+                    <input type="text" id="nasc" />
+                </div>
+                <div>
+                    <label for="cpf">CPF:</label>
+                    <input type="text" id="cpf" />
+                </div>
+                <div>
+                    <label for="rg">RG:</label>
+                    <input type="text" id="rg" />
+                </div>
+                <div>
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" />
+                </div>
+                <div>
+                    <label for="telResid">Telefone Residencial:</label>
+                    <input type="text" id="telResid" />
+                </div>
+                <div>
+                    <label for="telCel">Telefone Celular:</label>
+                    <input type="text" id="telCel" />
+                </div>
+                <div>
+                    <label for="endereco">Endereço:</label>
+                    <textarea id="endereco"></textarea>
+                </div>
+                <div>
+                    <label for="siape">SIAPE:</label>
+                    <input type="text" id="siape" />
+                </div>
+                <div>
+                <label for="cargo">Cargo:</label>
 
+                    <select name="cargo" id="cargo">
+                    <option value="Motorista">Motorista</option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    </select>
+                </div>
             </form>
 
         </div>
