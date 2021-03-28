f44378422b1067b02012616c7080b15d07a65e0b
thayssalisboa <77365773+thayssalisboa@users.noreply.github.com>
2021-03-28 17:58:49 -0300

    Update FormFunc.tsx

--- src/components/FormFunc.tsx
+++ src/components/FormFunc.tsx
@@ -15,39 +15,39 @@ export function FormFunc() {
             <form action="" method="post">
                 <div>
                     <label for="nome">Nome:</label>
-                    <input type="text" id="nome" />
+                    <input className={styles.formInput} type="text" id="nome" />
                 </div>
                 <div>
                     <label for="nasc">Data de nascimento:</label>
-                    <input type="text" id="nasc" />
+                    <input className={styles.formInput} type="text" id="nasc" />
                 </div>
                 <div>
                     <label for="cpf">CPF:</label>
-                    <input type="text" id="cpf" />
+                    <input className={styles.formInput} type="text" id="cpf" />
                 </div>
                 <div>
                     <label for="rg">RG:</label>
-                    <input type="text" id="rg" />
+                    <input className={styles.formInput} type="text" id="rg" />
                 </div>
                 <div>
                     <label for="email">E-mail:</label>
-                    <input type="email" id="email" />
+                    <input className={styles.formInput} type="email" id="email" />
                 </div>
                 <div>
                     <label for="telResid">Telefone Residencial:</label>
-                    <input type="text" id="telResid" />
+                    <input className={styles.formInput} type="text" id="telResid" />
                 </div>
                 <div>
                     <label for="telCel">Telefone Celular:</label>
-                    <input type="text" id="telCel" />
+                    <input className={styles.formInput} type="text" id="telCel" />
                 </div>
                 <div>
                     <label for="endereco">Endereço:</label>
-                    <textarea id="endereco"></textarea>
+                    <textarea className={styles.formInput} id="endereco"></textarea>
                 </div>
                 <div>
                     <label for="siape">SIAPE:</label>
-                    <input type="text" id="siape" />
+                    <input className={styles.formInput} type="text" id="siape" />
                 </div>
                 <div>
                 <label for="cargo">Cargo:</label>
@@ -65,18 +65,18 @@ export function FormFunc() {
                     <select name="setor" id="setor">
                     <option value="disciplina">Setor de disciplina</option>
                     <option value="admin">Gerência Administrativa</option>
-                    <option value="patrimonio">Seção de Patrimoôio</option>
+                    <option value="patrimonio">Seção de Patrimônio</option>
                     <option value="biblioteca">Biblioteca</option>
                     </select>
                 </div>
                 <div>
-                    <button type="submit">Enviar</button>
+                    <button className={styles.formBtnEnviar} type="submit">Enviar</button>
                 </div>
                 <div>
-                    <button type="submit">Cadastrar novo cargo</button>
+                    <button className={styles.formBtnCadastrar} type="submit">Cadastrar novo cargo</button>
                 </div>
                 <div>
-                    <button type="submit">Cadastrar novo setor</button>
+                    <button className={styles.formBtnCadastrar} type="submit">Cadastrar novo setor</button>
                 </div>
             </form>
 
