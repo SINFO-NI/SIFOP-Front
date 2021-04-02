145f5331a1a2f2c16416c556152c23156f4d47c8
thayssalisboa <thayssa.lisboa@aluno.cefet-rj.br>
2021-04-02 12:20:26 -0300

    Melhoria de CSS
    (tentativa)

--- src/components/FormFunc.tsx
+++ src/components/FormFunc.tsx
@@ -11,75 +11,78 @@ export function FormFunc() {
     return (
         
         <div className={styles.formContainer}>
+            <html>
+                <br />
 
-            <form action="" method="post">
-                <div>
-                    <label for="nome">Nome:</label>
-                    <input className={styles.formInput} type="text" id="nome" />
-                </div>
-                <div>
-                    <label for="nasc">Data de nascimento:</label>
-                    <input className={styles.formInput} type="text" id="nasc" />
-                </div>
-                <div>
-                    <label for="cpf">CPF:</label>
-                    <input className={styles.formInput} type="text" id="cpf" />
-                </div>
-                <div>
-                    <label for="rg">RG:</label>
-                    <input className={styles.formInput} type="text" id="rg" />
-                </div>
-                <div>
-                    <label for="email">E-mail:</label>
-                    <input className={styles.formInput} type="email" id="email" />
-                </div>
-                <div>
-                    <label for="telResid">Telefone Residencial:</label>
-                    <input className={styles.formInput} type="text" id="telResid" />
-                </div>
-                <div>
-                    <label for="telCel">Telefone Celular:</label>
-                    <input className={styles.formInput} type="text" id="telCel" />
-                </div>
-                <div>
-                    <label for="endereco">Endereço:</label>
-                    <textarea className={styles.formInput} id="endereco"></textarea>
-                </div>
-                <div>
-                    <label for="siape">SIAPE:</label>
-                    <input className={styles.formInput} type="text" id="siape" />
-                </div>
-                <div>
-                <label for="cargo">Cargo:</label>
-
-                    <select className={styles.formSelect} name="cargo" id="cargo">
-                    <option value="motorista">Motorista</option>
-                    <option value="assistenteSocial">Assistente Social</option>
-                    <option value="auxAdmin">Auxiliar Administrativo</option>
-                    <option value="nutricionista">Nutricionista</option>
-                    </select>
-                </div>
-                <div>
-                <label for="setor">Setor:</label>
-
-                    <select className={styles.formSelect} name="setor" id="setor">
-                    <option value="disciplina">Setor de disciplina</option>
-                    <option value="admin">Gerência Administrativa</option>
-                    <option value="patrimonio">Seção de Patrimônio</option>
-                    <option value="biblioteca">Biblioteca</option>
-                    </select>
-                </div>
-                <div>
-                    <button className={styles.formBtnEnviar} type="submit">Enviar</button>
-                </div>
-                <div>
-                    <button className={styles.formBtnCadastrar} type="submit">Cadastrar novo cargo</button>
-                </div>
-                <div>
-                    <button className={styles.formBtnCadastrar} type="submit">Cadastrar novo setor</button>
-                </div>
-            </form>
-
+                <form action="" method="post">
+                    <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="nome" placeholder="Nome" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="nasc" placeholder="Data de nascimento" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="cpf" placeholder="CPF" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="rg" placeholder="RG" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="email" id="email" placeholder="E-mail" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="telResid" placeholder="Telefone Residencial" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="telCel" placeholder="Telefone Celular" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <textarea className={styles.formInput2} id="endereco" placeholder="Endereço"></textarea>
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <input className={styles.formInput2} type="text" id="siape" placeholder="SIAPE" />
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <select className={styles.formSelect} name="cargo" id="cargo" placeholder="">
+                        <option value="">Cargo</option>
+                        <option value="motorista">Motorista</option>
+                        <option value="assistenteSocial">Assistente Social</option>
+                        <option value="auxAdmin">Auxiliar Administrativo</option>
+                        <option value="nutricionista">Nutricionista</option>
+                        </select>
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <select className={styles.formSelect} name="setor" id="setor">
+                        <option value="">Setor</option>
+                        <option value="disciplina">Setor de disciplina</option>
+                        <option value="admin">Gerência Administrativa</option>
+                        <option value="patrimonio">Seção de Patrimônio</option>
+                        <option value="biblioteca">Biblioteca</option>
+                        </select>
+                    </div>
+                    <br /> <br /> <br />
+                    <div>
+                        <button className={styles.formBtnEnviar} type="submit">Enviar</button>
+                    </div>
+                    <br />
+                    <div>
+                        <button className={styles.formBtnCadastrar} type="submit">Cadastrar novo cargo</button>
+                        <br /> <br /> <br />
+                        <button className={styles.formBtnCadastrar} type="submit">Cadastrar novo setor</button>
+                    </div>
+                </form>
+            </html>
         </div>
 
     );
